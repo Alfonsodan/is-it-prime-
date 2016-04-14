3c828220812bdc921f3f2195d0670c3bf732f50b
Alfonsodan <danalfons@yahoo.com>
2016-04-14 03:29:40 -0400

    broke it! ... swift 3??....

--- Is it Prime/ViewController.swift
+++ Is it Prime/ViewController.swift
@@ -9,7 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    @IBOutlet weak var resultsLabel: UILabel!
+    
+    @IBAction func isItPrime(sender: AnyObject) {
+        
+        let number = Int(numberTextField.text!)!
+        
+        var isPrime = true
+        
+        if number == 1 {
+            
+            isPrime = false
+        }
+        
+        if number != 2 && number != 1 {
+            
+            for _ in 2 ..< number {
+                
+                isPrime = false
+            }
+        }
+    }
+    
+    
+    if isPrime {
+    
+        resultLabel.text = "\(number) is prime!"
+    
+    } else {
+    
+        resultLabel.text =  "\(number) is not prime."
+    
+    } else {
+    
+        resultLabel.text = "Please enter a whole number."
+    }
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
